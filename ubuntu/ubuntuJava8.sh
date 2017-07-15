4ffc1afef26175e68a9317846aaa0e77d20a8d18
usman-tahir <tahir.usman.ali94@outlook.com>
2017-07-15 15:06:01 -0400

    updated the README and added a Java 8 installation script for Ubuntu

---
+++ ubuntu/ubuntuJava8.sh
@@ -0,0 +1,8 @@
+# install the Java 8 installer and run it
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt update; sudo apt install oracle-java8-installer
+
+# set Java 8 to the default for the machine
+sudo apt install oracle-java8-set-default
+
+
